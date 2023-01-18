7ea9e30386ce2cb86184eae2bd47b7a749cd7dd1
David Sanchez <tapi.cr7@gmail.com>
2023-01-18 00:49:21 -0300

    Iniciando challenge de Quinto impacto

---
+++ src/main/java/com/challenge/universidad/UniversidadApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.universidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversidadApplication.class, args);
+    }
+
+}
